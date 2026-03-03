72fb7a0c75212c97d8becda47ee6f97a43f03d17
Srinithy676 <sp7324@srmist.edu.in>
2026-03-03 11:10:05 +0530

    Use Case 3: Palindrome Checking Using String Reverse by srinithy

--- App/src/UseCase2PalindromeCheckerApp.java
+++ App/src/UseCase2PalindromeCheckerApp.java
@@ -1,32 +1,23 @@
-/**Use Case 2: Hardcoded Palindrome Validation
-Description:
-This class demonstrates basic palindrome validation
-        using a hardcoded string value..
-At this stoge, the application:
-Stores a predefined string
-Compares characters from both ends.
-Determines whether the string is a palindrome
-Displays the result on the console
-This use case introduces fundamental comparison logic
-before using advanced data structures.
+/**Use Case 3: Palindrome Checking Using String Reverse
 @author Developer
 @version 2.0**/
 import java.util.Scanner;
-public class UseCase2PalindromeCheckerApp{
+public class UseCase3PalindromeCheckerApp {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Input Text: ");
-        String str=sc.nextLine();
-        int n=str.length();
-        boolean isPalindrome=true;
-        for(int i=0;i<n/2;i++){
-            if(str.charAt(i)!=str.charAt(n-1-i)){
-                isPalindrome=false;
-                break;
-            }
+        System.out.println("Enter input: ");
+        String original=sc.nextLine();
+        String reversed="";
+        for(int i=original.length()-1;i>=0;i--){
+            reversed+=original.charAt(i);
         }
-        if (isPalindrome) System.out.println("is it a palindrome?: true");
-        else System.out.println("Is it a Palindrome?: False");
+        System.out.println("Reversed: "+reversed);
+        if(original.equals(reversed)){
+            System.out.println("It is a Palindrome Number");
+        }
+        else{
+            System.out.println("It is not a Palindrome Number");
+        }
         sc.close();
     }
 }
